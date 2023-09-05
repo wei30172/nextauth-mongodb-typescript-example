61ca29f2896a43508c1e32a98279438ea2c4a3ee
claireWei <claire610323@gmail.com>
2023-09-05 15:46:31 -0700

    signout button

--- components/button/SignOutButton.tsx
+++ components/button/SignOutButton.tsx
@@ -1,11 +1,15 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
+import { signOut } from 'next-auth/react'
 
 const SignOutButton = () => {
   const signout = async () => {
-    console.log('sign out')
-  };
+    signOut({
+      redirect: true,
+      callbackUrl: `${window.location.origin}/signin`
+    })
+  }
 
   return (
     <Button onClick={signout} variant='destructive'>
