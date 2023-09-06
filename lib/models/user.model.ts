8508677961db0de61d1de2a6241a0560b8d3ff5d
claireWei <claire610323@gmail.com>
2023-09-05 22:56:39 -0700

    setup mongoDB (mongoose)

---
+++ lib/models/user.model.ts
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  role: {
+    type: String,
+    default: 'user'
+  },
+  provider: {
+    type: String,
+    default: 'credentials'
+  }
+}, { timestamps: true })
+
+const User = mongoose.models.User || mongoose.model('User', userSchema)
+
+export default User
---
+++ lib/mongodb.ts
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+
+const connection: { isConnected?: number } = {}
+
+const connectDB = async () => {
+  if (connection.isConnected) {
+    return
+  }
+
+  if (!process.env.MONGODB_URI) {
+    console.error('Error: Invalid/Missing environment variable "MONGODB_URI"')
+    return
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI!)
+
+    connection.isConnected = db.connections[0].readyState
+    
+    if (connection.isConnected === 1) {
+      console.log('🚀 Successfully connected to database')
+    } else {
+      console.log('🔴 Failed to connect to database')
+    }
+  } catch (error) {
+    console.log('🔴 Failed to connect to MongoDB:', (error as Error).message)
+  }
+}
+
+export default connectDB
