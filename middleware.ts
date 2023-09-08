424057d973357690d77a8afda0007e645c7fc2c5
claireWei <claire610323@gmail.com>
2023-09-08 16:07:58 -0700

    middleware with auth

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,25 @@
-export { default } from 'next-auth/middleware'
+// export { default } from 'next-auth/middleware'
+import { withAuth } from 'next-auth/middleware'
+import { NextResponse } from 'next/server'
+
+export default withAuth(
+  function middleware(req) {
+    // console.log(req.nextauth.token)
+    console.log(req.nextUrl)
+    const { token } = req.nextauth
+    const { pathname } = req.nextUrl
+
+    if (pathname.startsWith('/dashboard') && token?.role !== 'admin') {
+      return new NextResponse('You Are Not Authorized!')
+    }
+  },
+  {
+    callbacks: {
+      // If `authorized` returns `true`, the middleware function will execute.
+      authorized: ({ token }) => !!token
+    },
+  }
+)
 
 export const config = {
   matcher: ['/dashboard/:path*', '/profile/:path*']
