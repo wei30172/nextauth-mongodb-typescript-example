29aa83b1ec910af1ccab6528734ca7bc49e47767
claireWei <claire610323@gmail.com>
2023-09-12 14:51:17 -0700

    update root layout

---
+++ providers/AuthProvider.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { SessionProvider } from 'next-auth/react';
+import { ReactNode } from 'react'
+
+interface ProviderProps {
+  children: ReactNode
+}
+
+export function AuthProvider({ children }: ProviderProps) {
+  return <SessionProvider>{children}</SessionProvider>
+}
