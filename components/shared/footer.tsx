0cdb27d215cc637feb86a0b5de471905307ddada
claireWei <claire610323@gmail.com>
2023-09-14 16:37:15 -0700

    update footer

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -6,7 +6,7 @@ import { Code } from "lucide-react"
 function Footer() {
   return (
     <footer className="flex flex-col text-gray-500 text-sm mt-5 border-t border-gray-100">
-      <div className="flex flex-wrap justify-between max-sm:flex-col gap-8 px-6 py-10 sm:gap-20">
+      <div className="flex flex-wrap justify-center max-sm:flex-col gap-10 px-6 py-10 sm:gap-20">
         {footerLinks.map((item) => (
           <div key={item.title}>
             <h3 className="font-semibold text-gray-900 dark:text-gray-200">{item.title}</h3>
@@ -25,14 +25,14 @@ function Footer() {
         ))}
       </div>
 
-      <div className="flex items-center justify-between flex-wrap border-t border-gray-100 gap-8 p-4 sm:px-10">
-        <div className="flex items-center gap-4">
+      <div className="flex items-center justify-between flex-wrap border-t border-gray-100 gap-8 px-6 py-4 sm:px-20">
+        <div className="flex items-center gap-2">
           <Code />
           <p className="text-gray-900 dark:text-gray-200">
             &copy; Web<br />
           </p>
         </div>
-        <div className="flex max-sm:flex-col gap-4">
+        <div className="flex max-sm:flex-col gap-2 sm:gap-4">
           <Link href="/">
             Privacy Notice
           </Link>
